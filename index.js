2f7fe799de1b42347cfa5ba5c94224ded0f03595
iliyan90 <iliyan.tarashev@gmail.com>
2023-03-24 17:50:37 +0300

    fist commit

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from 'express'
+const app = express();
+import http from 'http'
+const server = http.createServer(app);
+import cors from 'cors'
+import { Server } from 'socket.io';
+
+
+const io = new Server(server, {
+    cors: '*',
+    method: ["GET", "POST"]
+});
+app.use(cors());
+
+const PORT = 3001;
+
+app.get('/',(req, res) => {
+    res.send('Server is running')
+})
+
+io.on('connection', (socket) =>{
+    socket.emit('me', socket.id)
+    socket.on('disconnect', () =>{
+        socket.broadcast.emit('callended')
+    });
+    socket.on('calluser', ({userToCall, signalData, from, name}) =>{
+        io.to(userToCall).emit('calluser', {signal: signalData, from, name})
+    })
+    socket.on('answercall', (data) =>{
+        io.to(data.to).emit('callaccepted', data.signal);
+    })
+})
+
+server.listen(PORT, () =>{
+    console.log('connected to: ', PORT);
+})
+
